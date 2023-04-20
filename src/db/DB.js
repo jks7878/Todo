8f31a573082479c12b92597eee973608ae4510c2
jks7878 <67408890+jks7878@users.noreply.github.com>
2023-04-21 02:03:22 +0900

    userCheck unit test 수정

--- src/db/DB.js
+++ src/db/DB.js
@@ -1,7 +1,15 @@
 const mysql = require('mysql2/promise');
 const config = require('./dbconfig.json');
 
-const pool = mysql.createPool(config);
+const pool = mysql.createPool(config || {
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    connectionLimit: process.env.connectionLimit,
+    waitForConnections: process.env.waitForConnections
+});
 
 async function getConnection() {
     return await pool.getConnection(async conn => conn);
--- src/test/user.spec.js
+++ src/test/user.spec.js
@@ -1,44 +1,46 @@
 const { checkUserInfo, checkUserExist } = require('../middleware/userCheck');
+const todoUser = require('../repository/todoUser');
+jest.mock('../repository/todoUser');
 
-describe('userCheck - checkUserInfo Unit Test', () => {
-    test('req의 body가 비어있을 경우 400 error return', async () => {
-      const req = { body: {} };
-      const res = {};
-      const next = jest.fn();
-  
-      await checkUserInfo(req, res, next);
+describe('Middleware', () => {
+  describe('checkUserInfo', () => {
+    const res = {};
 
-      expect(next.mock.lastCall[0].code).toBe(400);
-      expect(next.mock.lastCall[0].message).toBe('Invalid Value of UserInfo');
-    });
-  
-    test('이미 존재하는 id로 가입 시도 시 409 error return', async () => {
+    it('should pass checkUserInfo middleware', async () => {
       const req = { body: { USER_ID: 'test', USER_PW: 'password' } };
-      const res = {};
       const next = jest.fn();
-  
+
       await checkUserInfo(req, res, next);
-  
-      expect(next.mock.lastCall[0].code).toBe(409);
-      expect(next.mock.lastCall[0].message).toBe('UserId is Already Exist');
-    });
-  
-    test('신규 유저 가입 시 next() 호출', async () => {
-      const req = { body: { USER_ID: 'newUser', USER_PW: 'password' } };
-      const res = {};
-      const next = jest.fn();
-  
-      await checkUserInfo(req, res, next);
-  
+
       expect(next).toHaveBeenCalledTimes(1);
     });
 
-    
-  });
+    it('should throw an error if USER_ID is already exist', async () => {
+      const req = { method: "POST", params: {}, body: { USER_ID: 'test', USER_PW: 'password' } };
+      const next = jest.fn();
 
-// describe("Test example", () => {
-//     test('GET /api/users', async () => {
-//         const response = await request(app).get('/api/users/0');        
-//         expect(response.body.code).toBe(404);
-//     });
-// });
+      const userCntRes = [{CNT: 1}];
+      jest.spyOn(todoUser, 'getTodoUserCnt').mockResolvedValue([userCntRes]);
+      await checkUserExist(req, res, next);
+  
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(
+        expect.objectContaining({ message:'UserId is Already Exist' })
+      );
+    });
+
+    it('should throw an error if request body is not provided', async () => {
+      const req = { body: {} };
+      const next = jest.fn();
+
+      await checkUserInfo(req, res, next);
+
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(
+        expect.objectContaining({ message:'Invalid Value of UserInfo' })
+      );
+    });
+
+
+  });
+});
